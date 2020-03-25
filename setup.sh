ba4562966261c78333d12bbbe21f818819157dc8
Harshad Reddy Nalla <hnalla@redhat.com>
2020-03-25 09:47:42 -0400

    scripts to setup and takedown pipeline
    Signed-off-by: Harshad Reddy Nalla <hnalla@redhat.com>

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-oc apply -f resource.yaml -n $1
-oc apply -f close-issue.yaml -n $1
-oc apply -f open-issue.yaml -n $1
-oc apply -f task.yaml -n $1
-oc apply -f pipeline.yaml -n $1
-oc apply -f triggertemplate.yaml -n $1
-oc apply -f triggerbinding.yaml -n $1
-oc apply -f eventlistener.yaml -n $1
+oc apply -f tasks/resource.yaml -n $1
+oc apply -f tasks/close-issue.yaml -n $1
+oc apply -f tasks/open-issue.yaml -n $1
+oc apply -f tasks/task.yaml -n $1
+oc apply -f pipeline/pipeline.yaml -n $1
+oc apply -f pipeline/triggertemplate.yaml -n $1
+oc apply -f pipeline/triggerbinding.yaml -n $1
+oc apply -f pipeline/eventlistener.yaml -n $1
--- teardown.sh
+++ teardown.sh
@@ -3,10 +3,10 @@ oc delete taskruns --all -n $1
 oc delete pipelineresource --all -n $1
 oc delete pipelinerun --all -n $1
 
-oc delete -f open-issue.yaml -n $1
-oc delete -f close-issue.yaml -n $1
-oc delete -f task.yaml -n $1
-oc delete -f pipeline.yaml -n $1
-oc delete -f triggertemplate.yaml -n $1
-oc delete -f triggerbinding.yaml -n $1
-oc delete -f eventlistener.yaml -n $1
+oc delete -f tasks/open-issue.yaml -n $1
+oc delete -f tasks/close-issue.yaml -n $1
+oc delete -f tasks/task.yaml -n $1
+oc delete -f pipeline/pipeline.yaml -n $1
+oc delete -f pipeline/triggertemplate.yaml -n $1
+oc delete -f pipeline/triggerbinding.yaml -n $1
+oc delete -f pipeline/eventlistener.yaml -n $1
