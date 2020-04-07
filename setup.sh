1f686adc8380f31630dfc9493ee794c828b8c23f
Harshad Reddy Nalla <hnalla@redhat.com>
2020-04-07 00:18:22 -0400

    setup and takedown scripts updated
    Signed-off-by: Harshad Reddy Nalla <hnalla@redhat.com>

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 oc apply -f tasks/resource.yaml -n $1
+
 oc apply -f tasks/close-issue.yaml -n $1
 oc apply -f tasks/open-issue.yaml -n $1
-oc apply -f tasks/task.yaml -n $1
+oc apply -f tasks/coala-checks.yaml -n $1
+oc apply -f tasks/pytest-checks.yaml -n $1
+oc apply -f tasks/pr-build.yaml -n $1
+
 oc apply -f pipeline/pipeline.yaml -n $1
 oc apply -f pipeline/triggertemplate.yaml -n $1
 oc apply -f pipeline/triggerbinding.yaml -n $1
--- teardown.sh
+++ teardown.sh
@@ -3,9 +3,12 @@ oc delete taskruns --all -n $1
 oc delete pipelineresource --all -n $1
 oc delete pipelinerun --all -n $1
 
-oc delete -f tasks/open-issue.yaml -n $1
 oc delete -f tasks/close-issue.yaml -n $1
-oc delete -f tasks/task.yaml -n $1
+oc delete -f tasks/open-issue.yaml -n $1
+oc delete -f tasks/coala-checks.yaml -n $1
+oc delete -f tasks/pytest-checks.yaml -n $1
+oc delete -f tasks/pr-build.yaml -n $1
+
 oc delete -f pipeline/pipeline.yaml -n $1
 oc delete -f pipeline/triggertemplate.yaml -n $1
 oc delete -f pipeline/triggerbinding.yaml -n $1
