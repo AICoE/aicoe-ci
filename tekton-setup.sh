462cd4609d64976028f63333997c3b943015a4fb
Harshad Reddy Nalla <hnalla@redhat.com>
2020-04-15 13:58:24 -0400

    Include thoth env vars in the builds
    Signed-off-by: Harshad Reddy Nalla <hnalla@redhat.com>

--- tekton-setup.sh
+++ tekton-setup.sh
@@ -4,10 +4,3 @@ oc apply --filename https://storage.googleapis.com/tekton-releases/pipeline/late
 oc apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
 oc apply --filename https://github.com/tektoncd/dashboard/releases/download/v0.5.2/tekton-dashboard-release.yaml
 oc expose svc/tekton-dashboard
-
-
-
-oc new-project tekton-demo
-oc create sa tekton-demo
-oc adm policy add-role-to-user edit -z tekton-demo -n tekton-demo
-oc adm policy add-scc-to-user privileged -z tekton-demo -n tekton-demo
