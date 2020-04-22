e5d833f8ec81ce92e6db322a16661ae2fbdc9261
Harshad Reddy Nalla <hnalla@redhat.com>
2020-04-22 09:25:03 -0400

    updated tekton dashboard
    Signed-off-by: Harshad Reddy Nalla <hnalla@redhat.com>

--- tekton-setup.sh
+++ tekton-setup.sh
@@ -2,5 +2,5 @@ oc new-project tekton-pipelines
 oc adm policy add-scc-to-user anyuid -z tekton-pipelines-controller
 oc apply --filename https://storage.googleapis.com/tekton-releases/pipeline/previous/v0.11.1/release.notags.yaml
 oc apply --filename https://storage.googleapis.com/tekton-releases/triggers/previous/v0.4.0/release.yaml
-oc apply --filename https://github.com/tektoncd/dashboard/releases/download/v0.5.2/tekton-dashboard-release.yaml
+oc apply --filename https://github.com/tektoncd/dashboard/releases/download/v0.6.0/tekton-dashboard-release.yaml
 oc expose svc/tekton-dashboard
